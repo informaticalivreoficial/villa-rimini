6d210d1c79c9b79ad3380fea32d1a17ae173a862
informaticalivreoficial <renatomontanari@ymail.com>
2024-11-22 22:48:46 -0300

    fix google analytics

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -31,7 +31,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //URL::forceScheme('https');
+        URL::forceScheme('https');
         Schema::defaultStringLength(191);
         Blade::aliasComponent('admin.components.message', 'message');
 
